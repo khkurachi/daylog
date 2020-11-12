1bd6a5513d9e9510e6650415b84aa56f686f1927
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-11-12 12:52:08 +0900

    微調整
    cssでclampを使ってみる
    @time=Time.nowをアプリ全体に適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_time
   private
+
+  def set_time
+    @time = Time.now
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
