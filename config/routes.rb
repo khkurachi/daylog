091538ae22181859d9d469cde213a4aaf0876e7b
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-11-18 11:10:04 +0900

    commentsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'days#index'
-  resources :days, only: [:index, :new, :create, :show, :update, :edit] do
+  resources :days, except: :destroy do
       get :search, on: :collection
+      resources :comments, only: :create
   end
 end
